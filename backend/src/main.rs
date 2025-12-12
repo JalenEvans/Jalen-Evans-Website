b84da91cc4fbca4a6b1877cfd791a6b6d197adfb
JalenEvans <jalenemmanuelevans@gmail.com>
2025-12-12 18:48:41 -0500

    Added cargo watch

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,7 +8,9 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run our app with hyper, listening locally on port 3000
+    let port = 3000;
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{port}")).await.unwrap();
+    println!("Listening at \"127.0.0.1:{0}\"", port);
     axum::serve(listener, app).await.unwrap();
 }
